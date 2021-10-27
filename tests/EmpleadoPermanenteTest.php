349a8e968d5bc839d8e3dfcc55f750d3eb16d56b
adrianocalonij <adrianocalonij@gmail.com>
2021-10-27 14:55:56 -0300

    Ver 0.07

--- tests/EmpleadoPermanenteTest.php
+++ tests/EmpleadoPermanenteTest.php
@@ -21,8 +21,24 @@
 		public function testCalcularComisionEnBaseALaAntiguedad(){
 			$ingreso = new DateTime();
 			$ingreso->modify('-10 years');
-			$ep= $this->crear('Emanuel','Montoto','30852963','5000', $ingreso); 
-			$this->assertEquals("10%",$ep->calcularComision());
+			$ep= $this->crear('Adriano','Caloni','36543024','15000', $ingreso); 
+			$this->assertEquals("15%",$ep->calcularComision());
+		}
+
+         //Tests 3/6: calcularIngresoTotal()
+		public function testSePuedeCalcularElIngresoTotal(){
+			$ingreso = new DateTime();
+			$ingreso->modify('-10 years');
+			$ep= $this->crear('Adriano','Caloni','36543024','15000', $ingreso); 
+			$this->assertEquals(16500,$ep->calcularIngresoTotal());
+		}
+
+		//Tests 4/6: calcularAntiguedad() 
+		public function testSePuedeCalcularAntiguedad(){
+			$ingreso = new DateTime();
+			$ingreso->modify('-10 years');
+			$ep= $this->crear('Adriano','Caloni','36543024','15000', $ingreso);
+			$this->assertEquals(10,$ep->calcularAntiguedad());
 		}
 
 		
